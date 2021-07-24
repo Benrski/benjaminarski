0d4c076f36273602e7acdbfedb334804870b9e17
Benrski <benjaminarski@gmail.com>
2021-07-24 21:04:54 +0200

    Added WindiCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
+import windiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh(), windiCSS()],
+});
