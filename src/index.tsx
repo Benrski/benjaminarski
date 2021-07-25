e65fddce57ddb112b6e5c33814aa91a3eec46243
Benrski <benjaminarski@gmail.com>
2021-07-25 22:08:08 +0200

    Removed Vite boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'virtual:windi.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <section className="flex justify-center items-center h-screen">
+      <div>
+        <p className="text-6xl">Hi there 👋</p>
+        <p className="text-8xl">I&apos;m Benjamin Minarski</p>
+      </div>
+    </section>
   </React.StrictMode>,
   document.getElementById('root')
 );
