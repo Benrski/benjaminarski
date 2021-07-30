f4e0b4071f28fad495729f89b712c32a40873971
Benrski <benjaminarski@gmail.com>
2021-07-30 18:31:08 +0200

    Added responsive text size

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,12 @@ ReactDOM.render(
   <React.StrictMode>
     <section className="flex justify-center items-center h-screen dark:(bg-gray-900 text-white)">
       <div>
-        <p className="text-6xl">Hi there 👋</p>
-        <p className="text-8xl">I&apos;m Benjamin Minarski</p>
+        <p className="text-lg sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+          Hi there 👋
+        </p>
+        <p className="text-2xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-10xl">
+          I&apos;m Benjamin Minarski
+        </p>
       </div>
     </section>
   </React.StrictMode>,
