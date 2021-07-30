b200c4c4f3e6225e3dd97bdeacf575173119eaa3
Benrski <benjaminarski@gmail.com>
2021-07-30 20:16:53 +0200

    Using vw for responsive text size

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,8 @@ ReactDOM.render(
   <React.StrictMode>
     <section className="flex justify-center items-center h-screen dark:(bg-gray-900 text-white)">
       <div>
-        <p className="text-lg sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
-          Hi there 👋
-        </p>
-        <p className="text-2xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-10xl">
-          I&apos;m Benjamin Minarski
-        </p>
+        <p className="text-[3vw]">Hi there 👋</p>
+        <p className="text-[6vw]">I&apos;m Benjamin Minarski</p>
       </div>
     </section>
   </React.StrictMode>,
