8120457b7b67b71f31d55ed7b81276dd3d245bc2
Benrski <benjaminarski@gmail.com>
2022-03-20 11:17:34 +0100

    Project reafctoring

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,7 @@ ReactDOM.render(
   <React.StrictMode>
     <section className="flex items-center justify-center h-screen dark:(bg-gray-900 text-white)">
       <div>
-        <p className="text-[4vw] leading-[5vw] xl:(text-[3vw] leading-[4vw])">
-          Hi there 👋
-        </p>
+        <p className="text-[4vw] leading-[5vw] xl:(text-[3vw] leading-[4vw])">Hi there 👋</p>
         <p className="text-[8vw] leading-[10vw] xl:(text-[6vw] leading-[8vw])">
           I&apos;m Benjamin Minarski
         </p>
