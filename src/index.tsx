6c371d902a18aaa8c4951102d4214b4ee085cfa1
Benrski <benjaminarski@gmail.com>
2022-03-20 16:11:09 +0100

    Replaced windicss with tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'virtual:windi.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <section className="flex items-center justify-center h-screen dark:(bg-gray-900 text-white)">
+    <section>
       <div>
-        <p className="text-[4vw] leading-[5vw] xl:(text-[3vw] leading-[4vw])">Hi there 👋</p>
-        <p className="text-[8vw] leading-[10vw] xl:(text-[6vw] leading-[8vw])">
-          I&apos;m Benjamin Minarski
-        </p>
+        <p>Hi there 👋</p>
+        <p>I&apos;m Benjamin Minarski</p>
       </div>
     </section>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import windiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), windiCSS()],
+  plugins: [react()],
 });
