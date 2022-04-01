ead3202d8a848386ccbd3bfc49b3102015b76d6e
Benrski <benjaminarski@gmail.com>
2022-04-01 20:27:41 +0200

    Moved waving hand in custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { animated, useChain, useSpring, useSpringRef } from '@react-spring/web';
 import React, { FunctionComponent } from 'react';
-import { animated, useChain, useSpring, useSpringRef } from 'react-spring';
+import { WavingHand } from './components/WavingHand';
 
 export const App: FunctionComponent = () => {
   const greetingSpringRef = useSpringRef();
@@ -7,14 +8,6 @@ export const App: FunctionComponent = () => {
     ref: greetingSpringRef,
     from: { opacity: 0 },
     to: { opacity: 1 },
-  });
-
-  const handSpring = useSpring({
-    config: { duration: 2000 },
-    loop: true,
-    from: { rotate: 0 },
-    rotate: 1,
-    transformOrigin: '75% 75%',
   });
 
   const nameSpringRef = useSpringRef();
@@ -44,19 +37,7 @@ export const App: FunctionComponent = () => {
     <section className="flex h-screen items-center justify-center p-8 dark:bg-neutral-900 dark:text-white">
       <animated.div style={containerSpring} className="space-y-2 md:space-y-3 lg:space-y-4">
         <animated.p style={greetingSpring} className="text-2xl md:text-3xl lg:text-4xl">
-          Hi there{' '}
-          <animated.span
-            className="inline-block"
-            style={{
-              ...handSpring,
-              rotate: handSpring.rotate.to({
-                range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1],
-                output: ['0deg', '16deg', '-8deg', '16deg', '-4deg', '8deg', '0deg', '0deg'],
-              }),
-            }}
-          >
-            👋
-          </animated.span>
+          Hi there <WavingHand />
         </animated.p>
 
         <animated.p style={nameSpring} className="text-4xl md:text-5xl lg:text-6xl">
---
+++ src/components/WavingHand.tsx
@@ -0,0 +1,28 @@
+import { animated, useSpring } from '@react-spring/web';
+import React, { FunctionComponent } from 'react';
+
+export const WavingHand: FunctionComponent = () => {
+  const handSpring = useSpring({
+    config: { duration: 2000 },
+    loop: true,
+    from: { rotate: 0 },
+    rotate: 1,
+    transformOrigin: '75% 75%',
+    immediate: true,
+  });
+
+  return (
+    <animated.span
+      className="inline-block"
+      style={{
+        ...handSpring,
+        rotate: handSpring.rotate.to({
+          range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1],
+          output: ['0deg', '16deg', '-8deg', '16deg', '-4deg', '8deg', '0deg', '0deg'],
+        }),
+      }}
+    >
+      👋
+    </animated.span>
+  );
+};
