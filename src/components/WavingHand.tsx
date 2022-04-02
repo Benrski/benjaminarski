4cf2694f2b08959665bec64d74f7021fce32fb11
Benrski <benjaminarski@gmail.com>
2022-04-02 10:31:22 +0200

    Replaced react-spring with @react-spring/web

--- src/components/WavingHand.tsx
+++ src/components/WavingHand.tsx
@@ -2,7 +2,7 @@ import { animated, useSpring } from '@react-spring/web';
 import React, { FunctionComponent } from 'react';
 
 export const WavingHand: FunctionComponent = () => {
-  const handSpring = useSpring({
+  const spring = useSpring({
     config: { duration: 2000 },
     loop: true,
     from: { rotate: 0 },
@@ -14,8 +14,8 @@ export const WavingHand: FunctionComponent = () => {
     <animated.span
       className="inline-block"
       style={{
-        ...handSpring,
-        rotate: handSpring.rotate.to({
+        ...spring,
+        rotate: spring.rotate.to({
           range: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1],
           output: ['0deg', '16deg', '-8deg', '16deg', '-4deg', '8deg', '0deg', '0deg'],
         }),
